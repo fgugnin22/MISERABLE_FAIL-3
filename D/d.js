0dfbc2d0ff08f7fe3e3a6233a207d936a42293af
fgugnin22 <fedorgugnin22@gmail.com>
2024-03-03 12:46:41 +0300

    The D

--- D/d.js
+++ D/d.js
@@ -14,27 +14,96 @@ for (let i = 0; i < matrix.length; i++) {
         }
     }
 }
+const isBetweenHorizontal = (x, y1, y2) => {
+    for (let i = y1 + 1; i <= y2 - 1; i++) {
+        if (matrix[x][i] !== "*") {
+            return true
+        }
+    }
+    return false
+}
+const isBetweenVertical = (y, x1, x2) => {
+    for (let i = x1 + 1; i <= x2 - 1; i++) {
+        if (matrix[i][y] !== "*") {
+            return true
+        }
+    }
+    return false
+}
+const isBetweenDiagonal = (x1, y1, x2, y2) => {
+    let directionX = 0;
+    let directionY = 0
+    if (x2 > x1) {
+        directionX = 1
+    } else {
+        directionX = -1
+    }
+    if (y2 > y1) {
+        directionY = 1
+    } else {
+        directionY = -1
+    }
+    for (let i = 1; i < Math.abs(x2 - x1); i++) {
+        if (matrix[x1 + i * directionX][y1 + i * directionY] !== "*") {
+            return true
+        }
+    }
+    return false
+}
+const isBetween = (x1, y1, x2, y2) => {
+    if (Math.abs(x1 - x2) === Math.abs(y1 - y2) && Math.abs(x1 - x2) <= 1) {
+        return false
+    }
+    if (x1 === x2) {
+        return isBetweenHorizontal(x1, Math.min(y1, y2), Math.max(y1, y2))
+    }
+    if (y1 === y2) {
+        return isBetweenVertical(y1, Math.min(x1, x2), Math.max(x1, x2))
+    }
+    if (x1 - x2 === y1 - y2 || x2 - x1 === y1 - y2) {
+        return isBetweenDiagonal(x1, y1, x2, y2)
+    }
+    return false
+}
+const isPotentialSlon = (i, j, sx, sy) => {
+    if (i - sx === j - sy || sx - i === j - sy) {
+        return true
+    }
+    return false
+}
+const isPotentialLad = (i, j, lx, ly) => {
+    if (i === lx || j === ly) {
+        return true
+    }
+    return false
+}
+const slon = (i, j) => {
+    for (let sl of sloni) {
+        if (isPotentialSlon(i, j, sl[0], sl[1])) {
+            if (!isBetween(i, j, sl[0], sl[1])) {
+                return true
+            }
+        }
+    }
+    return false
+}
+const ladya = (i, j) => {
+    for (let ld of ladi) {
+        if (isPotentialLad(i, j, ld[0], ld[1])) {
+            if (!isBetween(i, j, ld[0], ld[1])) {
+                return true
+            }
+        }
+    }
+    return false
+}
 let sum = 64
 for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
-        let flag = false
-        for (let s = 0; s < sloni.length; s++) {
-            if (i - sloni[s][0] === j - sloni[s][1] || i - sloni[s][0] === sloni[s][1] - j) {
-                sum--
-                flag = true
-                console.log(i, j)
-                break
-            }
+        if (slon(i, j) || ladya(i, j)) {
+            sum--;
         }
-        if (flag) { continue }
-        for (let l = 0; l < ladi.length; l++) {
-            if (ladi[l][0] === i || ladi[l][1] === j) {
-                sum--
-                console.log(i, j)
-                break
-            }
-        }
-
     }
 }
+// console.log(sum)
 fs.writeFileSync('output.txt', String(sum) + "\n")
